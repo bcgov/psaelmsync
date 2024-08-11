5997b28ed32ee0ef386653c9387b9536cc90a527
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-08-11 03:57:52 +0000

    Show the last run time on the dashboard.

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,8 @@ require_once('../../config.php');
 require_once($CFG->dirroot . '/local/psaelmsync/classes/log_table.php');
 require_login();
 
+global $DB;
+
 $context = context_system::instance();
 require_capability('local/psaelmsync:viewlogs', $context);
 
@@ -13,16 +15,17 @@ $PAGE->set_heading(get_string('logs', 'local_psaelmsync'));
 
 echo $OUTPUT->header();
 
-$apiurl = get_config('local_psaelmsync', 'apiurl');
+$sql = 'SELECT timecreated FROM {local_psaelmsync_runs} ORDER BY timecreated DESC LIMIT 1';
+$lastrun = $DB->get_field_sql($sql);
+echo 'Last run: ' . date('Y-m-d h:i:s', $lastrun);
 
+$apiurl = get_config('local_psaelmsync', 'apiurl');
 echo '<div class="my-3"><a href="/admin/settings.php?section=local_psaelmsync">Settings</a> | ';
 echo '<a href="' . $apiurl . '" target="_blank">Enrolment Endpoint</a> | ';
 echo '<a href="/local/psaelmsync/trigger_sync.php">Manually trigger intake</a></div>';
 
 $table = new \local_psaelmsync\output\log_table('psaelmsync_log_table');
-
 // Define the SQL query for fetching the logs.
-$sql = 'SELECT * FROM {local_psaelmsync_logs}';
 $countsql = 'SELECT COUNT(1) FROM {local_psaelmsync_logs}';
 $table->set_sql('*', '{local_psaelmsync_logs}', '1=1');
 
