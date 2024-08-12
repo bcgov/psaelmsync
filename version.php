2b69b71ac61c1a76cb147298b5e39f53e55ff294
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-08-12 21:37:12 +0000

    Reduce to Moodle 3.11.18 to see if it works there.

--- version.php
+++ version.php
@@ -2,7 +2,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2024081004;
-$plugin->requires  = 2022041900; // Moodle 4.2+.
+// $plugin->requires  = 2022041900; // Moodle 4.2+.
+$plugin->requires  = 2021051718; // Moodle 3.11+.
 $plugin->component = 'local_psaelmsync';
 $plugin->cron      = 600; // Run every 10 minutes.
 $plugin->maturity  = MATURITY_STABLE;
