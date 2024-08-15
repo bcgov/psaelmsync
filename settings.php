f4007b6a77663001bcd3f605fe7add4aa2254f06
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-08-15 04:33:13 +0000

    Date filter lang string update

--- settings.php
+++ settings.php
@@ -26,7 +26,7 @@ if ($hassiteconfig) {
     $settings->add(new admin_setting_configtext('local_psaelmsync/datefilterminutes', 
         get_string('datefilterminutes', 'local_psaelmsync'), 
         get_string('datefilterminutes_desc', 'local_psaelmsync'), 
-        '', PARAM_INT));
+        120, PARAM_INT));
 
     $settings->add(new admin_setting_configtext('local_psaelmsync/apiupdateurl', 
         get_string('apiupdateurl', 'local_psaelmsync'), 
