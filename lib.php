83d48a8b899d4b999ef79501fe27ddbc7adaacac
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-08-21 02:25:09 +0000

    Email formatting switch to heredoc.

--- lib.php
+++ lib.php
@@ -332,7 +332,14 @@ function suspend_user_in_course($user_id, $course_id) {
 
 function send_welcome_email($user, $course) {
     $subject = "Welcome to {$course->fullname}";
-    $message = "Dear {$user->firstname} {$user->lastname},\n\nYou have been enrolled in the course '{$course->fullname}'.\n\n'{$course->id}'.\n\nBest regards,\nPSA Learning Centre";
+    // $message = "Dear {$user->firstname} {$user->lastname},\n\nYou have been enrolled in the course '{$course->fullname}'.\n\n'{$course->id}'.\n\nBest regards,\nPSA Learning Centre";
+    $message = <<<EMAIL
+            Hello $user->firstname $user->lastname,\n\n
+            You have been enrolled in the course "$course->fullname".\n\n
+            https://learning.gww.gov.bc.ca/course/view.php?id=$course->id \n\n
+            Best regards,\n
+            PSA Learning Centre
+    EMAIL;
 
     email_to_user($user, core_user::get_support_user(), $subject, $message);
 }
