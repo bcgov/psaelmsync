bed18ea64b10eb9da2b1b0d3c9a4015948486a8b
Allan Haggett <Allan.Haggett@gov.bc.ca>
2024-09-05 00:23:22 +0000

    Annoying underscore got munged.

--- dashboard-courses.php
+++ dashboard-courses.php
@@ -5,7 +5,7 @@ require_login();
 global $DB, $OUTPUT, $PAGE;
 
 $context = context_system::instance();
-require_capability('local_psaelmsync:viewlogs', $context);
+require_capability('local/psaelmsync:viewlogs', $context);
 
 $PAGE->set_url('/local/psaelmsync/dashboard-courses.php');
 $PAGE->set_context($context);
